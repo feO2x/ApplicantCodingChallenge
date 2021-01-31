685428858170128712bd1e591079cc425de53f08
Kenny Pflug <kenny.pflug@live.de>
2021-01-31 19:23:41 +0100

    added EF Core with In-Memory Provider

---
+++ Hahn.ApplicationProcess.December2020.Data/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using Hahn.ApplicationProcess.December2020.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hahn.ApplicationProcess.December2020.Data
+{
+    public sealed class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Applicant> Applicants => Set<Applicant>();
+    }
+}
---
+++ Hahn.ApplicationProcess.December2020.Tests/InMemoryDatabaseIntegrationTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Hahn.ApplicationProcess.December2020.Data;
+using Hahn.ApplicationProcess.December2020.Domain;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Hahn.ApplicationProcess.December2020.Tests
+{
+    public static class InMemoryDatabaseIntegrationTests
+    {
+        private static DbContextOptions<DatabaseContext> Options { get; } =
+            new DbContextOptionsBuilder<DatabaseContext>().UseInMemoryDatabase("Test Applicants")
+                                                          .Options;
+
+        // This test is an ice breaker that checks if the EF In-Memory database behaves like a regular database
+        // and if the model default behavior is working as expected
+        [Fact]
+        public static async Task CreateAndEditApplicant()
+        {
+            DatabaseContext context;
+            Applicant newApplicant;
+            int numberOfAffectedRecords;
+            await using (context = new DatabaseContext(Options))
+            {
+                newApplicant = new Applicant
+                {
+                    FirstName = "Kenny",
+                    LastName = "Pflug",
+                    Address = "Würzburger Str. Regensburg",
+                    CountryOfOrigin = "Germany",
+                    EmailAddress = "kenny.pflug@live.de"
+                };
+                // ReSharper disable once MethodHasAsyncOverload
+                context.Applicants.Add(newApplicant);
+                numberOfAffectedRecords = await context.SaveChangesAsync();
+                numberOfAffectedRecords.Should().Be(1);
+            }
+
+            await using (context = new DatabaseContext(Options))
+            {
+                newApplicant = await context.Applicants.SingleOrDefaultAsync(applicant => applicant.LastName == "Pflug");
+                newApplicant.IsHired = true;
+                numberOfAffectedRecords = await context.SaveChangesAsync();
+                numberOfAffectedRecords.Should().Be(1);
+            }
+
+            await using (context = new DatabaseContext(Options))
+            {
+                newApplicant = await context.Applicants.SingleOrDefaultAsync(applicant => applicant.LastName == "Pflug");
+                var expectedApplicant = new Applicant
+                {
+                    Id = 1,
+                    FirstName = "Kenny",
+                    LastName = "Pflug",
+                    Address = "Würzburger Str. Regensburg",
+                    CountryOfOrigin = "Germany",
+                    EmailAddress = "kenny.pflug@live.de",
+                    IsHired = true
+                };
+                newApplicant.Should().BeEquivalentTo(expectedApplicant);
+            }
+        }
+    }
+}

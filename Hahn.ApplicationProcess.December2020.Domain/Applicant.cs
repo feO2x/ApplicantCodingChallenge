9ed07e3927cdb60fcb9dbec812d139f158a31a54
Kenny Pflug <kenny.pflug@live.de>
2021-01-31 13:26:44 +0100

    added Applicant entity

---
+++ Hahn.ApplicationProcess.December2020.Domain/Applicant.cs
@@ -0,0 +1,17 @@
+﻿namespace Hahn.ApplicationProcess.December2020.Domain
+{
+    public sealed class Applicant : Entity<Applicant>
+    {
+        public string FirstName { get; set; } = string.Empty;
+
+        public string LastName { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public string CountryOfOrigin { get; set; } = string.Empty;
+
+        public string EmailAddress { get; set; } = string.Empty;
+
+        public bool IsHired { get; set; }
+    }
+}

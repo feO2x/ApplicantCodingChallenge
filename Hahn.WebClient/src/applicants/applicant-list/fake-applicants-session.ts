f8b59b3829fa2e7440becdbaffaec6620fb4b48e
Kenny Pflug <kenny.pflug@live.de>
2021-02-12 20:20:31 +0100

    added loading indicator to list view

--- Hahn.WebClient/src/applicants/applicant-list/fake-applicants-session.ts
+++ Hahn.WebClient/src/applicants/applicant-list/fake-applicants-session.ts
@@ -5,7 +5,15 @@ import { generateFakeApplicants } from 'applicants/generate-fake-applicants';
 export class FakeApplicantsSession extends ApplicantsSession {
 
     getApplicants(skip: number, take: number, searchTerm?: string): Promise<Applicant[]> {
-        const appplicants = generateFakeApplicants(take, skip + 1);
-        return Promise.resolve(appplicants);
+        return new Promise<Applicant[]>(resolve => {
+            setTimeout(
+                () => {
+                    const applicants = generateFakeApplicants(take, skip + 1);
+                    resolve(applicants);
+                },
+                1500
+            );
+        });
+        
     }
 }

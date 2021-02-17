38a2b019a041835ee80e1807f3f92fe80ae8e7d0
Kenny Pflug <kenny.pflug@live.de>
2021-02-17 10:53:06 +0100

    added LanguageDetector

--- Hahn.WebClient/src/locales/configure-aurelia-i18n.ts
+++ Hahn.WebClient/src/locales/configure-aurelia-i18n.ts
@@ -1,16 +1,21 @@
 import { AureliaEnhancedI18Next, I18N, TCustomAttribute } from "aurelia-i18n";
 import i18nXhrBackend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 export function configureAureliaI18n(instance: I18N): Promise<AureliaEnhancedI18Next> {
     const aliases = ['t', 'i18n'];
     TCustomAttribute.configureAliases(aliases);
     instance.i18next.use(i18nXhrBackend);
+    instance.i18next.use(LanguageDetector);
 
     return instance.setup({
-        lng: 'de',
         fallbackLng: 'en',
         backend: {
             loadPath: './locales/{{lng}}.json' // must be relative to main.ts / from base URL
+        },
+        detection: {
+            order: [ 'localStorage', 'navigator' ],
+            lookupLocalStorage: 'language'
         },
         attributes: aliases,
         debug: true

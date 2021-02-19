bfcea8b3bedde6ecb74b720bc2ac75b5e1ab8f1d
Kenny Pflug <kenny.pflug@live.de>
2021-02-19 09:09:49 +0100

    added client-side validation for date of birth

--- Hahn.WebClient/src/applicants/applicant.ts
+++ Hahn.WebClient/src/applicants/applicant.ts
@@ -14,10 +14,28 @@ export interface Applicant extends ApplicantProperties {
     id: number;
 }
 
+export const applicantDateRangeRuleName = 'applicantDateRange';
+
+ValidationRules.customRule(
+    applicantDateRangeRuleName,
+    (value: string, _obj): boolean => {
+        console.warn(value);
+        const date = new Date(value);
+        if (isNaN(date.getTime()))
+            return false;
+        
+        const today = new Date();
+        const minimumDate = new Date("1900-01-01");
+        return minimumDate < date && date < today;
+    },
+    'The birth date is required and must be set to a value after 1900-01-01 and before today'
+);
+
 export const applicantValidationRules =
     ValidationRules
         .ensure((a: ApplicantProperties) => a.firstName).required().minLength(2).withMessageKey('invalidFirstName')
         .ensure((a: ApplicantProperties) => a.lastName).required().minLength(2).withMessageKey('invalidLastName')
+        .ensure((a: ApplicantProperties) => a.dateOfBirth).required().satisfiesRule(applicantDateRangeRuleName).withMessageKey('invalidDateOfBirth')
         .ensure((a: ApplicantProperties) => a.address).required().minLength(10).withMessageKey('invalidAddress')
         .ensure((a: ApplicantProperties) => a.emailAddress).required().email().withMessageKey('invalidEmail')
         .ensure((a: ApplicantProperties) => a.countryOfOrigin).required().withMessageKey('invalidCountry')
